57d4fa384aabe68aa5d7cb3f37d4302e981bd4ee
Luiz Henrique <luiz.andradesilva03@gmail.com>
2021-12-03 17:48:34 -0300

    AttBoard

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,6 @@
 
 const game = document.querySelector(".game");
+const body = document.querySelector("body")
 let board = ["","","","","","","","","",]
 const symbols = ["X","O"];
 let zeroAndOne = 0
@@ -11,23 +12,36 @@ changeIndex = ()=>{
 
 
 makeAplay = (squareNumber)=>{
-    if(board[squareNumber]==""){
-        squareSelected = document.querySelector(`.square-${squareNumber}`)
-        board[squareNumber] = symbols[zeroAndOne]
-        squareSelected.innerHTML = board[squareNumber]
-        changeIndex()
 
+    if(board[squareNumber]==""){
+        board[squareNumber] = symbols[zeroAndOne]
+        AttBoard()
+        changeIndex()
+        IsAWin()
     }
 }
 
-createBoard = ()=>{
-
+AttBoard = ()=>{
+    game.innerHTML = ""
     for(let i = 0; i < 9; i++){
         game.innerHTML +=  `<div onclick="makeAplay(${i})" class="square square-${i}">${board[i]}</div>`
     }
 
 }
 
-const winSequences = [
-    []
-]
+IsAWin =()=>{
+    if(
+        (board[0] == board[1] && board[0] == board[2]) && !(board[0] =="" ) ||
+        (board[3] == board[4] && board[3] == board[5]) && !(board[3] =="" ) ||
+        (board[6] == board[7] && board[6] == board[8]) && !(board[6] =="" ) ||
+        (board[0] == board[3] && board[0] == board[6]) && !(board[0] =="" ) ||
+        (board[1] == board[7] && board[1] == board[4]) && !(board[1] =="" ) ||
+        (board[2] == board[5] && board[2] == board[8]) && !(board[2] =="" ) ||
+        (board[0] == board[4] && board[0] == board[8]) && !(board[0] =="" ) ||
+        (board[2] == board[4] && board[6] == board[2]) && !(board[2] =="" ) 
+
+    ){
+        body.style.backgroundColor = "red"
+    }
+    
+}
