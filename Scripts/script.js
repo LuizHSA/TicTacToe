4f46f3686a05b83efabdcee2e06691e13ae4ce31
Luiz Henrique <luiz.andradesilva03@gmail.com>
2021-12-03 04:11:57 -0300

    Squares can be marked
    Clicks swap between "X" and "O" now

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,28 @@
 
 const board = document.querySelector(".board");
+const symbols = ["X","O"];
+let zeroAndOne = 0
 
+changeIndex = ()=>{
+    zeroAndOne = zeroAndOne === 0 ? 1 : 0
+}
 
+makeAplay = (squareNumber)=>{
+    squareSelected = document.querySelector(`.square-${squareNumber}`);
+    if(squareSelected.innerHTML ===""){
+        changeIndex()
+        squareSelected.innerHTML = symbols[zeroAndOne];
+    }
+}
 
 createBoard = ()=>{
 
     for(let i = 0; i < 9; i++){
-        board.innerHTML +=  `<div class="square ${i}">${i}</div>`
+        board.innerHTML +=  `<div onclick="makeAplay(${i})" class="square square-${i}"></div>`
     }
 
 }
 
+const winSequences = [
+    []
+]
